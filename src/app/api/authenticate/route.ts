3862f70d16266f6bfb3f7e4cec0fb4e1201d32bf
CadenMax <cadenmaxwell1701@gmail.com>
2025-11-28 08:56:56 +1000

    update runtime for non-static routes to be edge

--- src/app/api/authenticate/route.ts
+++ src/app/api/authenticate/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import * as cookie from "cookie";
+export const runtime = "edge";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
